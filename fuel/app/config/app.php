caf4e2328f7fefe3589598fab1022a002846d400
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-08 18:56:13 +0900

    config about db

--- fuel/app/config/app.php
+++ fuel/app/config/app.php
@@ -3,4 +3,4 @@
 return array(
     'site_name' => 'FuelPHP-app',
     'posts_per_page' => 10,
-)
+);
--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -6,5 +6,19 @@
  */
 
 return array(
+    'default' => array(
+        'type' => 'mysqli',
+        'connection' => array(
+            'hostname'   => 'db',
+            'database'   => 'fuelphp-db',
+            'username'   => 'root',
+            'password'   => 'root',
+            'persistent' => false,
+        ),
+        'table_prefix' => '',
+        'charset'      => 'utf8mb4',
+        'caching'      => false,
+        'profiling'    => true,
+    )
 
 );
