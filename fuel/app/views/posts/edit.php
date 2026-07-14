2df589b93fc70690cc5ba6d7162c3c335a2cdd67
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-14 15:26:35 +0900

    editをbootstrap化

--- fuel/app/views/posts/edit.php
+++ fuel/app/views/posts/edit.php
@@ -1,13 +1,54 @@
-<?php echo Form::open(array('action' => 'posts/edit/' . $post['id'], 'method' => 'post')); ?>
+<div class="card shadow-sm">
+    <div class="card-body">
 
-    <p>
-        <?php echo Form::label('Title', 'title'); ?>
-        <?php echo Form::input('title', $post['title']); ?>
-    </p>
-    <p>
-        <?php echo Form::label('Body', 'body'); ?>
-        <?php echo Form::textarea('body', $post['body']); ?>
-    </p>
-    <p>
-        <?php echo Form::submit('submit', 'Update'); ?>
-    </p>
+        <h1 class="h3 mb-4">投稿編集</h1>
+
+        <form action="<?php echo Uri::create('posts/edit/'.$post['id']); ?>" method="post">
+
+            <div class="mb-3">
+                <label for="title" class="form-label">
+                    タイトル
+                </label>
+
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    class="form-control"
+                    value="<?php echo e(Input::post('title', $post['title'])); ?>"
+                    required
+                >
+            </div>
+
+            <div class="mb-3">
+                <label for="body" class="form-label">
+                    本文
+                </label>
+
+                <textarea
+                    id="body"
+                    name="body"
+                    rows="8"
+                    class="form-control"
+                    required
+                ><?php echo e(Input::post('body', $post['body'])); ?></textarea>
+            </div>
+
+            <div class="d-flex gap-2">
+
+                <button type="submit" class="btn btn-primary">
+                    更新する
+                </button>
+
+                <?php echo Html::anchor(
+                    'posts/detail/'.$post['id'],
+                    'キャンセル',
+                    array('class' => 'btn btn-secondary')
+                ); ?>
+
+            </div>
+
+        </form>
+
+    </div>
+</div>
