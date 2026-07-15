7436cd21680805b9506ed89c981d0b2ff03b15e9
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-15 14:34:16 +0900

    add column remember_token to users table

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -11,6 +11,7 @@ return array(
         2 => '003_add_user_id_to_posts',
         3 => '004_drop_created_by_from_posts',
         4 => '005_recreate_users',
+        5 => '006_add_remember_token_to_users',
       ),
     ),
     'module' => 
