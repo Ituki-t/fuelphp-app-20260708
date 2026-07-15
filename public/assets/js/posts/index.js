06aea38fb0f786921549a40bcd09c021dd5b8701
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-15 11:06:33 +0900

    非同期処理？を実装

--- public/assets/js/posts/index.js
+++ public/assets/js/posts/index.js
@@ -3,9 +3,26 @@ document.addEventListener('DOMContentLoaded', function () {
         var self = this;
 
         self.keyword = ko.observable('');
+        self.posts = ko.observableArray([]);
 
         self.search = function () {
-            console.log('Searching for:', self.keyword());
+            var url = '/posts/search?keyword='
+                + encodeURIComponent(self.keyword());
+
+            fetch(url)
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(function (posts) {
+                    console.log('取得した投稿:', posts);
+                    self.posts(posts);
+                })
+                .catch(function (error) {
+                    console.error('Error fetching posts:', error);
+                });
         }
     }
 
